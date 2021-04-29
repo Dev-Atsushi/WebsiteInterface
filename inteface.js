43350976cc2892e7366cf005c73bc3373a438b79
Atsushi <atsushithesushi@gmail.com>
2021-04-29 09:16:29 -0300

    Update inteface.js

--- inteface.js
+++ inteface.js
@@ -1,22 +1,15 @@
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000
+const app     = express();
+const port    = process.env.PORT || 5000
 
-function checkHttps(req, res, next){
-  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
-    return next()
-} else {
-    res.redirect('https://' + req.hostname + req.url);
-}
-}                                                   // use this if you have a domain
-  app.all('*', checkHttps)
-  app.use(express.static("public"));                       // use this if you have a domain
- app.get("/", (request, response) => {
-   response.sendFile(__dirname + "/src/pages/home.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/pages/home.html");
 });
- app.get("/css/home", (request, response) => {
-   response.sendFile(__dirname + "/src/css/home.css");
+
+app.get("/css/home", (req, res) => {
+  req.sendFile(__dirname + "/src/css/home.css");
 });
+
 const listener = app.listen(port, () => {
   console.log(listener.address().port);
 });
